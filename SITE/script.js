cdc5e5c1261e1dccd75d23f2eee0036ef9aa9447
JonayRA <50452141+JonayRA@users.noreply.github.com>
2023-04-25 15:09:32 -0400

    PFD Format

---
+++ SITE/script.js
@@ -0,0 +1,109 @@
+// Comentario opcional para describir el contenido del archivo
+
+// Declaración de variables globales
+
+// Definición de funciones
+let PFD = document.getElementById('PFD');
+let ctxPFD = PFD.getContext('2d');
+let ctxFMA1 = FMA1.getContext('2d');
+let FMA2 = document.getElementById('FMA2');
+let ctxFMA2 = FMA2.getContext('2d');
+let FMA3 = document.getElementById('FMA3');
+let ctxFMA3 = FMA3.getContext('2d');
+let FMA4 = document.getElementById('FMA4');
+let ctxFMA4 = FMA4.getContext('2d');
+let FMA5 = document.getElementById('FMA5');
+let ctxFMA5 = FMA5.getContext('2d');
+let ASI = document.getElementById('ASI');
+let ctxASI = ASI.getContext('2d');
+let HDG = document.getElementById('HDG');
+let ctxHDG = HDG.getContext('2d');
+let VSI = document.getElementById('VSI');
+let ctxVSI = VSI.getContext('2d');
+let ALT = document.getElementById('ALT');
+let ctxALT = ALT.getContext('2d');
+let AH = document.getElementById('AH');
+let ctxAH = AH.getContext('2d');
+
+
+// Texto secciones PFD
+var t1 = 'FMA';
+var t2 = 'ASI';
+var t3 = 'HDG';
+var t4 = 'VSI';
+var t5 = 'AH';
+var t6 = 'ALT';
+ctxFMA3.font = '30px Arial';
+var textWidth = ctxFMA3.measureText(t1).width;
+var x = (FMA3.width / 2) - textWidth/2;
+var y = FMA3.height / 2;
+ctxFMA3.fillStyle = 'white';
+ctxFMA3.fillText(t1,x,y);
+ctxASI.font = '60px Arial';
+var textWidth2 = ctxASI.measureText(t2).width;
+var x2 = (ASI.width / 2) - textWidth2/2;
+var y2 = ASI.height / 2;
+ctxASI.fillStyle = 'white';
+ctxASI.fillText(t2,x2,y2);
+ctxAH.font = '30px Arial';
+var textWidth3 = ctxAH.measureText(t5).width;
+var x3 = (AH.width / 2) - textWidth3/2;
+var y3 = AH.height / 2;
+ctxAH.fillStyle = 'white';
+ctxAH.fillText(t5,x3,y3);
+ctxHDG.font = '30px Arial';
+var textWidth4 = ctxHDG.measureText(t3).width;
+var x4 = (HDG.width / 2) - textWidth4/2;
+var y4 = HDG.height / 2;
+ctxHDG.fillStyle = 'white';
+ctxHDG.fillText(t3,x4,y4);
+ctxVSI.font = '60px Arial';
+var textWidth5 = ctxVSI.measureText(t4).width;
+var x5 = (VSI.width / 2) - textWidth5/2;
+var y5 = VSI.height/2;
+ctxVSI.fillStyle = 'white';
+ctxVSI.fillText(t4,x5,y5);
+ctxALT.font = '60px Arial';
+var textWidth = ctxALT.measureText(t6).width;
+var x = (ALT.width / 2) - textWidth/2;
+var y = ALT.height / 2;
+ctxALT.fillStyle = 'white';
+ctxALT.fillText(t6,x,y);
+
+
+
+
+let ND = document.getElementById('ND');
+let ctxND = ND.getContext('2d');
+
+let pixelRatio = 10 // window.devicePixelRatio || 1;
+PFD.width = 200 * pixelRatio;
+PFD.height = 200 * pixelRatio;
+ctxPFD.scale(pixelRatio, pixelRatio);
+ND.width = 200*pixelRatio
+ND.height = 200*pixelRatio
+ctxND.scale(pixelRatio,devicePixelRatio)
+
+ctxPFD.fillStyle = 'black';
+ctxPFD.fillRect(0, 0, PFD.width, PFD.height);
+
+ctxND.fillStyle = 'black';
+ctxND.fillRect(0, 0, ND.width, ND.height);
+
+let imgAH = new Image();
+imgAH.src = "D:/DOCUMENTOS/DOCTORADO THALES/DOCTORADO/EXPERIENCIAS/CELLULE ORAGEUSSE - ETUDIANTES UL/PRUEBA ARRASTRAR OBJETOS/Test_GUI/Test_GUI2/SITE/IMAGENES/AH.png";
+imgAH.onload = function() {
+  ctxAH.drawImage(imgAH, 0, 0, AH.width, AH.height);
+}
+imgAH.onerror = function() {
+  console.log('Error al cargar la imagen');
+}
+
+/*
+ctxPFD.beginPath();
+ctxPFD.moveTo(0,0);
+ctxPFD.lineTo(PFD.width,PFD.height);
+ctxPFD.strokeStyle = 'white';
+ctxPFD.stroke(); */
+
+// Código principal que utiliza las variables y funciones definidas anteriormente
